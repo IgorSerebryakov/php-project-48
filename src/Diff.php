a135ce69337f224a9602d476c4ce4fcf7f60d631
IgorSerebryakov <serebryakov09@mail.ru>
2024-01-07 18:36:17 +0300

    create genDiff func

--- src/Diff.php
+++ src/Diff.php
@@ -2,41 +2,35 @@
 
 namespace Differ\Differ;
 
-$autoloadPath1 = __DIR__ . '/../../../autoload.php';
-$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
-
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
-} else {
-    require_once $autoloadPath2;
-}
-
 use function Functional\sort;
 
-function genDiff($firstPath, $secondPath)
+function genDiff($firstFilePath, $secondFilePath, $format = 'stylish')
 {
-    
+    $dataFromFirstFile = getContest($firstFilePath);
+    $dataFromSecondFile = getContest($secondFilePath);
+
+    $decodedDataFromFirstFile = getJsonDecode($dataFromFirstFile);
+    $decodedDataFromSecondFile = getJsonDecode($dataFromSecondFile);
+
+    return getDiff($decodedDataFromFirstFile, $decodedDataFromSecondFile);
 }
 
-function getDiff($dataFromFirstFile, $dataFromSecondFile)
+function getDiff(array $dataFromFirstFile, array $dataFromSecondFile)
 {
-    $decodedFirstFile = getJsonDecode($dataFromFirstFile);
-    $decodedSecondFile = getJsonDecode($dataFromSecondFile);
-
-    $mergedFiles = array_merge($decodedFirstFile, $decodedSecondFile);
+    $mergedFiles = array_merge($dataFromFirstFile, $dataFromSecondFile);
     $keys = array_keys($mergedFiles);
     $sortedKeys = sort($keys, fn($left, $right) => strcmp($left, $right));
 
-    $diff = array_reduce($sortedKeys, function ($acc, $key) use ($decodedFirstFile, $decodedSecondFile) {
-        if (!array_key_exists($key, $decodedSecondFile)) {
-            $acc[] = "  - {$key}: " . getValue($decodedFirstFile[$key]);
-        } elseif (!array_key_exists($key, $decodedFirstFile)) {
-            $acc[] = "  + {$key}: " . getValue($decodedSecondFile[$key]);
-        } elseif ($decodedFirstFile[$key] !== $decodedSecondFile[$key]) {
-            $acc[] = "  - {$key}: " . getValue($decodedFirstFile[$key]);
-            $acc[] = "  + {$key}: " . getValue($decodedSecondFile[$key]);
+    $diff = array_reduce($sortedKeys, function ($acc, $key) use ($dataFromFirstFile, $dataFromSecondFile) {
+        if (!array_key_exists($key, $dataFromSecondFile)) {
+            $acc[] = "  - {$key}: " . getValue($dataFromFirstFile[$key]);
+        } elseif (!array_key_exists($key, $dataFromFirstFile)) {
+            $acc[] = "  + {$key}: " . getValue($dataFromSecondFile[$key]);
+        } elseif ($dataFromFirstFile[$key] !== $dataFromSecondFile[$key]) {
+            $acc[] = "  - {$key}: " . getValue($dataFromFirstFile[$key]);
+            $acc[] = "  + {$key}: " . getValue($dataFromSecondFile[$key]);
         } else {
-            $acc[] = "    {$key}: " . getValue($decodedFirstFile[$key]);
+            $acc[] = "    {$key}: " . getValue($dataFromFirstFile[$key]);
         }
 
         return $acc;
@@ -45,44 +39,17 @@ function getDiff($dataFromFirstFile, $dataFromSecondFile)
     return "{\n" . implode("\n", $diff) . "\n}";
 }
 
-$firstPath = '{
-    "host": "hexlet.io",
-    "timeout": 50,
-    "proxy": "123.234.53.22",
-    "follow": false
-}';
-
-$secondPath = '{
-    "timeout": 20,
-    "verbose": true,
-    "host": "hexlet.io"
-}';
-
-//gendiff filepath1.json filepath2.json
-//
-//{
-//    - follow: false
-//      host: hexlet.io
-//    - proxy: 123.234.53.22
-//    - timeout: 50
-//    + timeout: 20
-//    + verbose: true
-//}
-
 function getJsonDecode($file)
 {
     return json_decode($file, true);
 }
-
-function getJsonEncode($file)
-{
-    return json_encode($file);
-}
-
-print_r(getDiff($firstPath, $secondPath));
 
 function getValue($value)
 {
     return is_bool($value) ? var_export($value, true) : $value;
 }
 
+function getContest($path)
+{
+    return file_get_contents($path);
+}
