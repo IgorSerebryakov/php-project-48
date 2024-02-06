fbe977b788cdb7fe2774878858e814e5bc17bd6d
IgorSerebryakov <serebryakov09@mail.ru>
2024-02-06 23:12:18 +0300

    fix linter

--- formatters/Plain.php
+++ formatters/Plain.php
@@ -6,21 +6,23 @@ use function Functional\flatten;
 
 function getPlain(array $tree, string $key = '')
 {
-    $coll = array_map(function ($val) use ($key) {
-        $currentKey = $key === "" ? "{$val['key']}" : "{$key}.{$val['key']}";
-        
-        if ($val['status'] === 'added') {
-            return "Property " . "'{$currentKey}'" . " was added with value: " . getValue($val['value']);
-        } elseif ($val['status'] === 'deleted') {
-            return "Property " . "'{$currentKey}'" . " was removed";
-        } elseif ($val['status'] === 'changed') {
-            return "Property " . "'{$currentKey}'" . " was updated. From " . getValue($val['oldValue']) .
+    $coll = array_map(
+        function ($val) use ($key) {
+            $currentKey = $key === "" ? "{$val['key']}" : "{$key}.{$val['key']}";
+            if ($val['status'] === 'added') {
+                return "Property " . "'{$currentKey}'" . " was added with value: " . getValue($val['value']);
+            } elseif ($val['status'] === 'deleted') {
+                return "Property " . "'{$currentKey}'" . " was removed";
+            } elseif ($val['status'] === 'changed') {
+                return "Property " . "'{$currentKey}'" . " was updated. From " . getValue($val['oldValue']) .
                 " to " . getValue($val['newValue']);
-        } elseif ($val['status'] === 'root') {
-            $children = $val['children'];
-            return getPlain($children, $currentKey);
-        }
-    }, $tree);
+            } elseif ($val['status'] === 'root') {
+                $children = $val['children'];
+                return getPlain($children, $currentKey);
+            }
+        },
+        $tree
+    );
 
     return implode("\n", array_filter(flatten($coll)));
 }
